1884d83e5e8701245b26c9a2884f80caf4721d5d
Tshifhiwa Sinugo <tshifhiwa@amrod.co.za>
2026-04-19 00:13:52 +0200

    chore: update create driver to handle run in ci

--- src/main/java/com/tshifhiwa/configuration/driver/BrowserManager.java
+++ src/main/java/com/tshifhiwa/configuration/driver/BrowserManager.java
@@ -1,5 +1,6 @@
 package com.tshifhiwa.configuration.driver;
 
+import com.tshifhiwa.configuration.environment.EnvironmentDetector;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
@@ -18,21 +19,31 @@ public class BrowserManager {
 
     public static WebDriver createDriver(String browser) {
         logger.info("Creating driver [browser={}]", browser);
+        boolean isCI = EnvironmentDetector.isCI();
 
         switch (browser.toLowerCase()) {
             case "chrome":
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("--start-maximized");
+                if (isCI) {
+                    chromeOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+                }
                 return new ChromeDriver(chromeOptions);
 
             case "firefox":
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
                 firefoxOptions.addArguments("--start-maximized");
+                if (isCI) {
+                    firefoxOptions.addArguments("--headless");
+                }
                 return new FirefoxDriver(firefoxOptions);
 
             case "edge":
                 EdgeOptions edgeOptions = new EdgeOptions();
                 edgeOptions.addArguments("--start-maximized");
+                if (isCI) {
+                    edgeOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+                }
                 return new EdgeDriver(edgeOptions);
 
             default:
